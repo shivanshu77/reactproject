dcf37dec2ded26c09d1d09229b0a995970f22a50
Shivanshu7 <161882443+shivanshu77@users.noreply.github.com>
2024-05-27 11:08:36 +0530

    navbar update

---
+++ Navbar.tsx
@@ -0,0 +1,26 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="" />
+        <span>Shivanshu</span>
+      </div>
+      <div className="icons">
+        
+        
+          
+          <span>1</span>
+        </div>
+        <div className="user">
+          
+          <span>Shivanshu</span>
+        </div>
+
+      </div>
+    
+  );
+};
+
+export default Navbar;
